b8e9c6e3884f524527bee3d2535300b8dca0e07d
Michael Lawrenson <ctrlcmdspace@gmail.com>
2019-09-18 02:06:33 -0400

    Added playbook for creating log directories

--- ansible-scraper/full_install.sh
+++ ansible-scraper/full_install.sh
@@ -1,6 +1,9 @@
-ansible-playbook deploy.yml --extra-vars Name=iced-coffee --tags system
-ansible-playbook deploy.yml --extra-vars Name=iced-coffee --tags env
-ansible-playbook deploy.yml --extra-vars Name=iced-coffee --tags database
-ansible-playbook deploy.yml --extra-vars Name=iced-coffee --tags app
-ansible-playbook deploy.yml --extra-vars Name=iced-coffee --tags server
-ansible-playbook deploy.yml --extra-vars Name=iced-coffee --tags test
+TAG_NAME=iced-coffee
+ansible-playbook deploy.yml --extra-vars Name=$TAG_NAME --tags system
+ansible-playbook deploy.yml --extra-vars Name=$TAG_NAME --tags env
+ansible-playbook deploy.yml --extra-vars Name=$TAG_NAME --tags database
+ansible-playbook deploy.yml --extra-vars Name=$TAG_NAME --tags broker
+ansible-playbook deploy.yml --extra-vars Name=$TAG_NAME --tags app
+ansible-playbook deploy.yml --extra-vars Name=$TAG_NAME --tags celery
+ansible-playbook deploy.yml --extra-vars Name=$TAG_NAME --tags server
+ansible-playbook deploy.yml --extra-vars Name=$TAG_NAME --tags test
